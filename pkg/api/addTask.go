d903588af56279e3fb9cbeb054ecd09a6c1d201a
solitudo <zbisliyev@mail.ru>
2026-01-15 01:29:02 +0300

    final-project

--- pkg/api/addTask.go
+++ pkg/api/addTask.go
@@ -43,38 +43,7 @@ func checkDate(task *db.Task) error {
 
 	return nil
 }
-// const format = "20060102"
 
-// func checkDate(task *db.Task, now time.Time) error {
-//  if task.Date == "" {
-//   task.Date = now.Format(format)
-//   return nil
-//  }
-
-//  t, err := time.Parse(format, task.Date)
-//  if err != nil {
-//   return err
-//  }
-
-//  today := time.Date(
-//   now.Year(), now.Month(), now.Day(),
-//   0, 0, 0, 0, now.Location(),
-//  )
-
-//  if t.Before(today) {
-//   if task.Repeat == "" {
-//    task.Date = today.Format(format)
-//   } else {
-//    next, err := NextDate(now, task.Date, task.Repeat)
-//    if err != nil {
-//     return err
-//    }
-//    task.Date = next
-//   }
-//  }
-
-//  return nil
-// }
 
 func addTaskHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
